5ec9c0cb03aa2eb49944b7eb47e3a261bf138e90
realbewhy <44467425+realbewhy@users.noreply.github.com>
2022-10-11 17:12:06 +0700

    Update

--- src/components/research/History.js
+++ src/components/research/History.js
@@ -32,12 +32,12 @@ const History = () => {
     {
       key: "startTime",
       label: "Start time",
-      format: (e) => moment(e).utc().format("HH:mm:ss YYYY-MM-DD"),
+      format: (e) => moment(e).format("HH:mm:ss YYYY-MM-DD"),
     },
     {
       key: "endTime",
       label: "End time",
-      format: (e) => moment(e).utc().format("HH:mm:ss YYYY-MM-DD"),
+      format: (e) => moment(e).format("HH:mm:ss YYYY-MM-DD"),
     },
     {
       key: "",
