43b2925e065335e564343fa9c16e6ff1aa0989ce
realbewhy <44467425+realbewhy@users.noreply.github.com>
2022-11-09 09:39:47 +0700

    -- ri time by FE

--- src/store/actions/riActions.js
+++ src/store/actions/riActions.js
@@ -4,16 +4,24 @@ import { get } from "../../utils/api";
 import { ReduxConstant } from "../constants";
 
 export const _getRICountdown = () => (dispatch) => {
-  get(EndpointConstant.NFT_RE_COUNTDOWN, (data) => {
-    if (data[0]) {
-      console.log(
-        "Serer end time:",
-        moment(data[0].lockTime).format("YYYY-MM-DD hh:mm:ss")
-      );
-      dispatch({
-        type: ReduxConstant.GET_RI_COUNTDOWN,
-        payload: data[0].lockTime,
-      });
-    }
+  let now = moment().utcOffset(0);
+  now.set({ hour: 0, minute: 0, second: 0, millisecond: 0 });
+  now = (now.unix() + 24 * 60 * 60) * 1000;
+  console.log("R-I reset time:", moment(now).utc().toISOString(), now);
+  dispatch({
+    type: ReduxConstant.GET_RI_COUNTDOWN,
+    payload: now,
   });
+  // get(EndpointConstant.NFT_RE_COUNTDOWN, (data) => {
+  //   if (data[0]) {
+  //     console.log(
+  //       "Serer end time:",
+  //       moment(data[0].lockTime).format("YYYY-MM-DD hh:mm:ss")
+  //     );
+  //     dispatch({
+  //       type: ReduxConstant.GET_RI_COUNTDOWN,
+  //       payload: data[0].lockTime,
+  //     });
+  //   }
+  // });
 };
