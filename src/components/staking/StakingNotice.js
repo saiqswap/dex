0a06a3a3f9a56c67f1a27af1dfc01c77ac08e4a6
realbewhy <44467425+realbewhy@users.noreply.github.com>
2022-11-10 11:39:14 +0700

    -- update

--- src/components/staking/StakingNotice.js
+++ src/components/staking/StakingNotice.js
@@ -7,6 +7,15 @@ const CustomLI = styled("li")(({ theme }) => ({
     marginBottom: theme.spacing(1),
   },
 }));
+
+const notices = [
+  "Unstaking fee - 1% of the staking principal is and unstaking fee will be burn.",
+  "Interest is paid daily. At the end of the staking period, the system automatically unstakes.",
+  "4 slots open when staking 40,000 ING. To open 6 slots, you need to stake 120,000 ING.",
+  "When unstaking, slot number 4 / number 5 / number 6 will be withdrawn when the staking balance does not meet the amount of ING specified in the slots.",
+  "Staking duration with corresponding benefits is different. After the unstake is completed, you will receive the corresponding benefit within up to 7 days.",
+];
+
 export default function StakingNotice() {
   return (
     <>
@@ -16,28 +25,11 @@ export default function StakingNotice() {
           listStyleType: "unset",
         }}
       >
-        <CustomLI>
-          Unstaking fee - 1% of the staking principal is and unstaking fee will
-          be burn.
-        </CustomLI>
-        <CustomLI>
-          Interest is paid daily. At the end of the staking period, the system
-          automatically unstakes.
-        </CustomLI>
-        <CustomLI>
-          4 slots open when staking 40,000 ING. To open 6 slots, you need to
-          stake 120,000 ING.
-        </CustomLI>
-        <CustomLI>
-          When unstaking, slot number 4 / number 5 / number 6 will be withdrawn
-          when the staking balance does not meet the amount of ING specified in
-          the slots.
-        </CustomLI>
-        <CustomLI>
-          Staking duration with corresponding benefits is different. After the
-          unstake is completed, you will receive the corresponding benefit
-          within up to 7 days.
-        </CustomLI>
+        {notices.map((notice, index) => (
+          <CustomLI key={index}>
+            <Typography sx={{ opacity: 0.8 }}>{notice}</Typography>
+          </CustomLI>
+        ))}
       </ul>
     </>
   );
